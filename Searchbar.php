eaf1cf9c1c5e14077e2259381acd14a5c8309136
unknown <roehan.abdul-rauf@laplateforme.io>
2022-04-14 14:11:37 +0200

    modif searchbar

--- Searchbar.php
+++ Searchbar.php
@@ -2,32 +2,30 @@
 
 class Searchbar extends Config
 {
-   
     public function BarreDeRecherche($q)
     {
+        // echo 1;
+        // $produits = $this->bdd->query("SELECT blaze, prix, description FROM produits ORDER BY id DESC");
         echo 1;
-        $produits = $this->bdd->query("SELECT nom, prix, description FROM produits ORDER BY id DESC");
+        $req = $this->bdd->prepare('SELECT blaze, prix, description FROM produits WHERE blaze LIKE "%' . $q . '%" ORDER BY id DESC');
+        $req->execute();
+        $produits = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        var_dump($produits);
 
         if (!empty($q)) {
             echo 2;
-            $produits = $this->bdd->query('SELECT nom, prix, description FROM produits WHERE nom LIKE "%'.$q.'%" ORDER BY id DESC');
-
-            if ($produits->rowCount() == 0) {
+            if (count($produits)) {
                 echo 3;
-                $produits = $this->bdd->query('SELECT nom, prix, description FROM produits WHERE CONCAT(nom, description) LIKE "%'.$q.'%" ORDER BY id DESC');
-            }
-        } elseif ($produits->rowCount() > 0) { 
-            echo 4;?>
-        
-            <?php while ($a = $produits->fetch()) { ?>
-                <li><?= $a['nom'] ?>
-                <?= $a['description'] ?>
-                <?= $a['prix'] ?> <br /></1i>
-                <?php } ?>
-                </ul>
-            <?php } else { ?>
-                Aucun résultat pour: <?= $q ?>...
-            <?php } 
-     }
-
-}
+                foreach ($produits as $a) { ?>
+                    <li><?= $a['blaze'] ?>
+                        <?= $a['description'] ?>
+                        <?= $a['prix'] ?> <br /></1i>
+                    <?php } ?>
+                    </ul>
+                <?php } else { echo 4 ?>
+                    Aucun résultat pour: <?= $q ?>...<?php
+                                                    }
+                                                }
+                                            }
+                                        }
