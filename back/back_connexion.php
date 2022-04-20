5dc23e28d3fb681a85b21cd35fcf461757308aa0
mohamed-zidi <mohamed.zidi@laplateforme.io>
2022-04-20 15:06:52 +0200

    ajustement inscription et connexion

---
+++ back/back_connexion.php
@@ -0,0 +1,6 @@
+<?php
+
+if(isset($_POST['pageInscription'])){
+
+header('location:../index.php?page=inscription');
+}
--- inscription.php
+++ inscription.php
@@ -1,4 +1,5 @@
 <?php
+
 // require('config.php');
 // require('User.php');
 
@@ -32,79 +33,64 @@
         $home -> headerStyle();
     ?>
     <main>
-        <div class="padding-top2 padding-left4 padding-bottom3">
+    <div class="">
             <button><a href="index.php">Retour a l'Accueil</a></button>
         </div>
-        <div align="center">
-            <form method="POST">
                 <?php if (isset($_POST['inscription'])) {
-                    
+
                     $user = new User();
                     $user->Register(htmlspecialchars($_POST['prenom']),htmlspecialchars($_POST['nom']),htmlspecialchars($_POST['mail']) , htmlspecialchars($_POST['adresse']),htmlspecialchars($_POST['codepostal']),htmlspecialchars($_POST['ville']),htmlspecialchars($_POST['password']), htmlspecialchars($_POST['passwordverify']));
                     $user->alerts();
                 }
                 ?>
-                <table class="form">
-                    <tr>
-                        <td>
-                            <h1 class="h_1">Bonjour, je suis nouveau ici. <br>Je crée mon compte</h1>
-                            <label>Prénom*</label>
-                            <input type="text" name="prenom" placeholder="Entrer votre prénom" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Nom*</label>
-                            <input type="text" name="nom" placeholder="Entrer votre nom" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Adresse email*</label>
-                            <input type="email" name="mail" placeholder="Entrer votre adresse email" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Adresse*</label>
-                            <input type="text" name="adresse" placeholder="Entrer votre adresse" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Code Postal*</label>
-                            <input type="text" minlength="5" maxlength="5" name="codepostal" placeholder="Entrer votre code postal" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Ville*</label>
-                            <input type="text" name="ville" placeholder="Entrer votre ville" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Mot de passe*</label>
-                            <input type="password" name="password" placeholder="Entrer un mot de passe" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>Confirmation du mot de passe*</label>
-                            <input type="password" name="passwordverify" placeholder="Confirmer le mot de passe" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="center">
-                            <button class="button" type="submit" name="inscription">M'inscrire</button>
-                        </td>
-                    </tr>
-                </table>
+
+        <div align="center">
+        <form class="forms" method="POST" action="./back/back_inscription.php">
+        <h1 class="titres">Bonjour</h1>
+        <button type="submit" class="btn2" name="pageDeConnexion">Se connecter</button>
+        </form>
+        </div>
+        <hr>
+        <div align="center">
+            <form class="forms" method="POST">
+                            <h1 class="titres">Je suis nouveau ici</h1>
+                            <h4 class="input-inscription">Prénom*</h4>
+                            <input class="input-field" type="text" name="prenom" placeholder="Entrez votre prénom" required>
+                            <h4 class="input-inscription2">Nom*</h4>
+                            <input class="input-field" type="text" name="nom" placeholder="Entrez votre nom" required>
+                            <h4 class="input-inscription3">Adresse email*</h4>
+                            <input class="input-field" type="email" name="mail" placeholder="Entrez votre adresse email" required>
+                            <h4 class="input-inscription">Adresse*</h4>
+                            <input class="input-field" type="text" name="adresse" placeholder="Entrez votre adresse" required>
+                            <h4 class="input-inscription4">Code postal*</h4>
+                            <input class="input-field" type="text" minlength="5" maxlength="5" name="codepostal" placeholder="Entrez votre code postal" required>
+                            <h4 class="input-inscription5">Ville*</h4>
+                            <input class="input-field" type="text" name="ville" placeholder="Entrez votre ville" required>
+                            <h4 class="input-inscription6">Mot de passe*</h4>
+                            <input class="input-field" type="password" name="password" placeholder="Entrez un mot de passe" required>
+                            <h4 class="input-inscription7">Confirmation de mot de passe</h4>
+                            <input class="input-field" type="password" name="passwordverify" placeholder="Confirmez le mot de passe" required>
+
+                        <div align="center">
+                            <button class="btn" type="submit" name="inscription">S'inscrire</button>
             </form>
+        </div>
+        <div align="center">
+        <div class="space-between">
+        <div class="conditions">
+        Politique de confidentialité
+        </div>
+        <div class="conditions">
+        Conditions d’utilisation
+        </div>
+        <div class="conditions">
+        Mentions légales
+        </div>
+        </div>
         </div>
         <footer>
         <div>
-            <p class="scoreXcopyright padding-top3 ">Copyright ©2022 . Tous droits réservés.</p>
+            <p class=" ">Copyright ©2022 . Tous droits réservés.</p>
         </div>
                 </footer>
     </main>
