56f620c9fb0c7537601d80de26bef43229485d76
khellaf-rachedi <91748769+khellaf-rachedi@users.noreply.github.com>
2022-04-22 13:01:05 +0200

    ajout pages à propos de nous & footer responsive

---
+++ responsive-footer-design.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>responsive footer</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style media="screen">
+
+    </style>
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <h6 class="titre-footer">Los watches Hermanos </h6>
+            <p>Bonjour et bienvenue sur Los Watches hermanos, le site où trouver les modèles les plus incroyables de montres pour tous les passionnés de la haute horlogerie.</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright 2022&copy; <a href="#">Los watches Hermanos</a>  </p>
+                    <div class="footer-menu">
+                      <ul class="f-menu">
+                        <li><a href="aboutus.html">Qui sommes-nous?</a></li>
+                      </ul>
+                    </div>
+        </div>
+
+    </footer>
+</body>
+</html>
